f9468ba71845d73df41c41e114f5bf8b1f5162e4
scastros <scastros@users.noreply.github.com>
2023-04-14 23:50:11 +0200

    Fashion button

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,23 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+const CONTROLS = document.querySelectorAll("button");
+const UPDATE = ({ x, y }) => {
+
+  const ELEMENT = document.elementFromPoint(x, y);
+  const CONTROL = ELEMENT.closest(".control");
+  if (CONTROL) {
+    console.info('doing it')
+    const BOUNDS = CONTROL.getBoundingClientRect()
+    CONTROL.style.setProperty("--rx", (x - BOUNDS.x) / BOUNDS.width);
+    CONTROL.style.setProperty("--x", (x - BOUNDS.x) / BOUNDS.width);
+    CONTROL.style.setProperty("--y", (y - BOUNDS.y) / BOUNDS.height);
+  }
+};
+
+document.body.addEventListener("pointermove", UPDATE);
+    
+
 (function($) {
 
 	var settings = {
